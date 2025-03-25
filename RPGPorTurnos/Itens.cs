12a3df13ee62d20535a067331cea34faed92448c
Luca Lee <lucalee2019@gmail.com>
2025-03-24 21:11:30 -0300

    Segundo Commit

--- RPGPorTurnos/Itens.cs
+++ RPGPorTurnos/Itens.cs
@@ -12,7 +12,7 @@ namespace RPGPorTurnos
             equipamentos[0, 0] = "ITEM";
             equipamentos[0, 1] = "TIPO";
             equipamentos[0, 2] = "VALOR";
-            equipamentos[0, 3] = "DANO/CURA";
+            equipamentos[0, 3] = "DANO/CURA/BÔNUS";
             equipamentos[0, 4] = "DEFESA";
             equipamentos[0, 5] = "ALCANCE";
             equipamentos[0, 6] = "SLOTS";
@@ -59,7 +59,7 @@ namespace RPGPorTurnos
             equipamentos[5, 2] = "15";
             equipamentos[5, 3] = "15 Cura";
             equipamentos[5, 4] = "0";
-            equipamentos[5, 5] = "Curto";
+            equipamentos[5, 5] = "Corpo a Corpo";
             equipamentos[5, 6] = "2 (Duas Mãos)";
             equipamentos[5, 7] = "Sem Classe/Bardo";
 
@@ -90,12 +90,122 @@ namespace RPGPorTurnos
             equipamentos[7, 6] = "1";
             equipamentos[7, 7] = "Sem Classe";
 
+            equipamentos[9, 0] = "Poção de Força";
+            equipamentos[9, 1] = "Consumível";
+            equipamentos[9, 2] = "5";
+            equipamentos[9, 3] = "+5 de Ataque Físico/Distância (Bônus)";
+            equipamentos[9, 4] = "0";
+            equipamentos[9, 5] = "Corpo a Corpo (Consurmir)/Curto (Arremessar)";
+            equipamentos[9, 6] = "1";
+            equipamentos[9, 7] = "Sem Classe";
+
+            equipamentos[10, 0] = "Poção de Força Grande";
+            equipamentos[10, 1] = "Consumível";
+            equipamentos[10, 2] = "15";
+            equipamentos[10, 3] = "+20 de Ataque Físico/Distância (Bônus)";
+            equipamentos[10, 4] = "0";
+            equipamentos[10, 5] = "Corpo a Corpo (Consurmir)/Curto (Arremessar)";
+            equipamentos[10, 6] = "1";
+            equipamentos[10, 7] = "Sem Classe";
+
+            equipamentos[11, 0] = "Poção de Mana";
+            equipamentos[11, 1] = "Consumível";
+            equipamentos[11, 2] = "5";
+            equipamentos[11, 3] = "+5 de Ataque Mágico (Bônus)";
+            equipamentos[11, 4] = "0";
+            equipamentos[11, 5] = "Corpo a Corpo (Consurmir)/Curto (Arremessar)";
+            equipamentos[11, 6] = "1";
+            equipamentos[11, 7] = "Mago/Bardo";
+
+            equipamentos[12, 0] = "Poção de Mana Grande";
+            equipamentos[12, 1] = "Consumível";
+            equipamentos[12, 2] = "15";
+            equipamentos[12, 3] = "+20 de Ataque Mágico (Bônus)";
+            equipamentos[12, 4] = "0";
+            equipamentos[12, 5] = "Corpo a Corpo (Consurmir)/Curto (Arremessar)";
+            equipamentos[12, 6] = "1";
+            equipamentos[12, 7] = "Mago/Bardo";
+
+            equipamentos[13, 0] = "Aljava de Flechas";
+            equipamentos[13, 1] = "Munição";
+            equipamentos[13, 2] = "5";
+            equipamentos[13, 3] = "5 (Arremessar/Sem Arco)";
+            equipamentos[13, 4] = "0";
+            equipamentos[13, 5] = "Médio (Arremessar/Sem Arco)";
+            equipamentos[13, 6] = "2";
+            equipamentos[13, 7] = "Sem Classe/Arqueiro";
+
+            equipamentos[14, 0] = "Armadura de Guerreiro";
+            equipamentos[14, 1] = "Armadura";
+            equipamentos[14, 2] = "30";
+            equipamentos[14, 3] = "0";
+            equipamentos[14, 4] = "30";
+            equipamentos[14, 5] = "Nenhuma";
+            equipamentos[14, 6] = "15";
+            equipamentos[14, 7] = "Guerreiro";
+
+            equipamentos[15, 0] = "Manto de Couro Batido";
+            equipamentos[15, 1] = "Armadura";
+            equipamentos[15, 2] = "30";
+            equipamentos[15, 3] = "+15 de Ataque Físico/Distância (Bônus)";
+            equipamentos[15, 4] = "15";
+            equipamentos[15, 5] = "Nenhuma";
+            equipamentos[15, 6] = "5";
+            equipamentos[15, 7] = "Arqueiro";
+
+            equipamentos[16, 0] = "Tûnica de Mago";
+            equipamentos[16, 1] = "Armadura";
+            equipamentos[16, 2] = "30";
+            equipamentos[16, 3] = "+20 de Ataque Mágico (Bônus)";
+            equipamentos[16, 4] = "10";
+            equipamentos[16, 5] = "Nenhuma";
+            equipamentos[16, 6] = "5";
+            equipamentos[16, 7] = "Mago";
+
+            equipamentos[17, 0] = "";
+            equipamentos[17, 1] = "";
+            equipamentos[17, 2] = "";
+            equipamentos[17, 3] = "";
+            equipamentos[17, 4] = "";
+            equipamentos[17, 5] = "";
+            equipamentos[17, 6] = "";
+            equipamentos[17, 7] = "Monge";
+
+            equipamentos[18, 0] = "";
+            equipamentos[18, 1] = "";
+            equipamentos[18, 2] = "";
+            equipamentos[18, 3] = "";
+            equipamentos[18, 4] = "";
+            equipamentos[18, 5] = "";
+            equipamentos[18, 6] = "";
+            equipamentos[18, 7] = "Bardo";
+
+            equipamentos[19, 0] = "";
+            equipamentos[19, 1] = "";
+            equipamentos[19, 2] = "";
+            equipamentos[19, 3] = "";
+            equipamentos[19, 4] = "";
+            equipamentos[19, 5] = "";
+            equipamentos[19, 6] = "";
+            equipamentos[19, 7] = "Ladino";
+
+            equipamentos[20, 0] = "Zênite";
+            equipamentos[20, 1] = "Arma";
+            equipamentos[20, 2] = "480153.98";
+            equipamentos[20, 3] = "314159265359";
+            equipamentos[20, 4] = "0";
+            equipamentos[20, 5] = "Médio";
+            equipamentos[20, 6] = "2";
+            equipamentos[20, 7] = "Guerreiro";
+
+
             listarItens();
         }
 
         public void listarItens()
         {
             Console.WriteLine(equipamentos);
+            Console.WriteLine("TESTE");
         }
     }
 }
--- RPGPorTurnos/obj/Debug/netcoreapp2.1/RPGPorTurnos.AssemblyInfo.cs
+++ RPGPorTurnos/obj/Debug/netcoreapp2.1/RPGPorTurnos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGPorTurnos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+049ad890abb97a42b4d82eeb4b62904ccacb8f98")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGPorTurnos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGPorTurnos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
